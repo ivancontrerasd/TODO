02d35294fabc7e3cbfe2f5a735bece476018f775
ivancontrerasd <ivan.duarte@cetys.edu.mx>
2018-03-15 20:55:37 -0700

    Archivo subido

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCfds9yu5k5WX6zaha2w6sINqCZ-GhvrGM",
+    authDomain: "todolistapp-af472.firebaseapp.com",
+    databaseURL: "https://todolistapp-af472.firebaseio.com",
+    projectId: "todolistapp-af472",
+    storageBucket: "todolistapp-af472.appspot.com",
+    messagingSenderId: "193143383415"
+  }
 };
